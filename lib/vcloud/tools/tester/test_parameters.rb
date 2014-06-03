b14c6d46571311890383d7d9f7ec887a30f409fa
Matt Bostock <matt@mattbostock.com>
2014-06-03 12:52:03 +0100

    Dynamically generate param attribute readers
    Dynamically generate attribute reader methods for the test parameters
    read in from the YAML-formatted configuration file.
    
    Implementing these methods dynamically means that we don't need to
    manually add a new method and bump the version of this gem each time we
    change test parameters in the integration tests.
    
    Note that we're using `send` because `define_method` is a private method
    and we're calling it from inside the class initalizer.

--- lib/vcloud/tools/tester/test_parameters.rb
+++ lib/vcloud/tools/tester/test_parameters.rb
@@ -23,86 +23,10 @@ module Vcloud
           @input_config = all_config.fetch(organization) do
             raise "Invalid FOG_CREDENTIAL value '#{organization}'"
           end
-        end
 
-        def vdc_1_name
-          @input_config["vdc_1_name"]
-        end
-
-        def vdc_2_name
-          @input_config["vdc_2_name"]
-        end
-
-        def catalog
-          @input_config["catalog"]
-        end
-
-        def vapp_template
-          @input_config["vapp_template"]
-        end
-
-        def network_1
-          @input_config["network_1"]
-        end
-
-        def network_1_id
-          @input_config["network_1_id"]
-        end
-
-        def network_1_ip
-          @input_config["network_1_ip"]
-        end
-
-        def network_2
-          @input_config["network_2"]
-        end
-
-        def network_2_id
-          @input_config["network_2_id"]
-        end
-
-        def network_2_ip
-          @input_config["network_2_ip"]
-        end
-
-        def storage_profile
-          @input_config["storage_profile"]
-        end
-
-        def default_storage_profile_name
-          @input_config["default_storage_profile_name"]
-        end
-
-        def default_storage_profile_href
-          @input_config["default_storage_profile_href"]
-        end
-
-        def vdc_1_storage_profile_href
-          @input_config["vdc_1_storage_profile_href"]
-        end
-
-        def vdc_2_storage_profile_href
-          @input_config["vdc_2_storage_profile_href"]
-        end
-
-        def edge_gateway
-          @input_config["edge_gateway"]
-        end
-
-        def edge_gateway_id
-          @input_config["edge_gateway_id"]
-        end
-
-        def provider_network
-          @input_config["provider_network"]
-        end
-
-        def provider_network_id
-          @input_config["provider_network_id"]
-        end
-
-        def provider_network_ip
-          @input_config["provider_network_ip"]
+          @input_config.each_key do |param|
+            self.class.send(:define_method, param) { @input_config[param] }
+          end
         end
       end
     end
